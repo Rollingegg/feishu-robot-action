c7e26d94fafb92bb26937e305864a00d78caf4f7
Rollingegg <1244797125@qq.com>
2021-09-14 17:53:17 +0800

    fix: test

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,15 @@ import { createTextMessage, FRobot } from './robot'
 
 async function run(): Promise<void> {
   try {
-    const uuid: string = core.getInput('uuid')
+    const uuid = core.getInput('uuid')
     const version = core.getInput('version')
     const secret = core.getInput('secret')
     const text = core.getInput('text')
     const json = core.getInput('json')
-    core.debug(json)
+    core.info(json)
 
     const robot = new FRobot(uuid, version, secret)
-    // if (!text && !json) throw new Error('invalid data')
+    if (!text && !json) throw new Error('invalid data')
     if (text) {
       const res = await robot.send(createTextMessage(text))
       core.debug(res)
